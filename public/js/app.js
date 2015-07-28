391cc99185c19d4c3b7ee2594eb4bfa738cc4dc0
fraserk <kimfraser@gmail.com>
2015-07-28 18:05:29 -0400

    remove port

--- public/js/app.js
+++ public/js/app.js
@@ -8,7 +8,7 @@ new Vue ({
 
         self = this;
         // var socket = io('http://realgram.dev:3000');
-         var socket = io('http://real.picblocks.com');
+         var socket = io();
          socket.on("test-channel:App\\Events\\EventName", function(message){
              // increase the power everytime we load test route
              self.instagrams.push(message.data);
