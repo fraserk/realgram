b5eff2b5c5f1f7d9c174a7a4d0d39ed357e4a884
fraserk <kimfraser@gmail.com>
2015-07-28 19:43:50 -0400

    add name space

--- public/js/app.js
+++ public/js/app.js
@@ -9,7 +9,7 @@ new Vue ({
         self = this;
         // var socket = io('http://realgram.dev:3000');
          var socket = io('http://real.picblocks.com:3000');
-         socket.on("test-channel:EventName", function(message){
+         socket.on("test-channel:App\\Events\\EventName", function(message){
              // increase the power everytime we load test route
              self.instagrams.push(message.data);
 
