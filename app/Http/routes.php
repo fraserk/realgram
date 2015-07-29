b0de3c8220f05d9a2fab8843c0ef74ca012a0088
fraserk <kimfraser@gmail.com>
2015-07-28 22:04:40 -0400

    geting ver

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -71,9 +71,9 @@ Route::get('/callback', function(Request $Request){
 });
 
 Route::post('/callback', function(Request $Request){
- $requestData = $Request->all();
- $object_id = $requestData->data['object_id'];
- $id        = $requestData->data['id'];
+ $requestData = $Request->get('data');
+ $object_id = $requestData['object_id'];
+ $id        = $requestData['id'];
  $client = new GuzzleHttp\Client();
  $response = $client->get('https://api.instagram.com/v1/tags/nofilter/media/recent?client_id=ba86e397e3e7471a9909aaf1bdb93010&max_id=19266176');
 
