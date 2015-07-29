98f486e20b3e3c15de1ed735953201a880fd63fa
fraserk <kimfraser@gmail.com>
2015-07-29 07:07:33 -0400

    remove min id

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -74,7 +74,7 @@ Route::post('/callback', function(Request $Request){
  $object_id = $requestData['object_id'];
  $id        = $requestData['id'];
  $client = new GuzzleHttp\Client();
- $response = $client->get('https://api.instagram.com/v1/tags/nofilter/media/recent?client_id=ba86e397e3e7471a9909aaf1bdb93010&max_id='.$id);
+ $response = $client->get('https://api.instagram.com/v1/tags/nofilter/media/recent?client_id=ba86e397e3e7471a9909aaf1bdb93010');
 
  //dd($data);
  //dd($data->data['0']->images->standard_resolution->url);
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,8 +3,8 @@
         <section id="instagram">
             <h1>Images </h1>
             <ul>
-                <li v-repeat="t : instagrams">
-                    ---<img src=@{{t.instagram}} alt="" /> 
+                <li v-repeat="t : instagrams | orderBy 'instagram' -1"">
+                    ---<img src=@{{t.instagram}} alt="" width="20" />
 
                 </li>
             </ul>
