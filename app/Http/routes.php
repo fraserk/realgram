8e0dcb14c5f0741ecf35ac44c24785e31541b923
fraserk <kimfraser@gmail.com>
2015-07-28 22:32:38 -0400

    re

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -45,7 +45,7 @@ Route::get('/instagram', function(){
         'client_secret' => getenv('ClientSecret'),
         'aspect' => "media",
         'object' => "tag",
-        'object_id' => "NYC",
+        'object_id' => "nofilter",
         'callback_url' => 'http://real.picblocks.com/callback'
     );
 
@@ -74,7 +74,7 @@ Route::post('/callback', function(Request $Request){
  $object_id = $requestData['object_id'];
  $id        = $requestData['id'];
  $client = new GuzzleHttp\Client();
- $response = $client->get('https://api.instagram.com/v1/tags/'.$object_id .'/media/recent?client_id=ba86e397e3e7471a9909aaf1bdb93010&max_id' .$id);
+ $response = $client->get('https://api.instagram.com/v1/tags/nofilter/media/recent?client_id=ba86e397e3e7471a9909aaf1bdb93010&max_id=19266176');
 
  //dd($data);
  //dd($data->data['0']->images->standard_resolution->url);
