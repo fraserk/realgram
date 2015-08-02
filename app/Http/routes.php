3e4054af7f4025d7a10db8d49a7dc67193c04f8f
fraserk <kimfraser@gmail.com>
2015-08-02 16:03:02 -0400

    d

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -82,8 +82,12 @@ Route::post('/callback', function(Request $Request){
     $id        = $k['id'];
     $response = $client->get('https://api.instagram.com/v1/tags/nofilter/media/recent?client_id=ba86e397e3e7471a9909aaf1bdb93010');
     $instadata = json_decode($response->getBody()->getContents());
-    $data = $instadata->data;
-    event(new App\Events\EventName($data));
+    $returndata = $instadata->data;
+    foreach ($returndata as $data) {
+
+         event(new App\Events\EventName($data));
+
+        }
      }
 
  //dd($data);
