be73019fdd9f77d58487883ec0fd14e2c6dff710
fraserk <kimfraser@gmail.com>
2015-08-02 11:46:54 -0400

    remove collection

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -82,9 +82,8 @@ Route::post('/callback', function(Request $Request){
     $id        = $k['id'];
     $response = $client->get('https://api.instagram.com/v1/tags/nofilter/media/recent?client_id=ba86e397e3e7471a9909aaf1bdb93010');
     $instadata = json_decode($response->getBody()->getContents());
-    $returndata = collect($instadata->data);
+    $returndata = $instadata->data;
     foreach ($returndata as $data) {
-         Log::info($returndata);
          event(new App\Events\EventName($data));
 
         }
